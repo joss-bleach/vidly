7381dd2eb015be54a3b284e607c19088bd4bd817
Joss Bleach <joss.bleach@outlook.com>
2025-02-09 18:28:19 +0000

    added infinite scrolling and formatted videos in studio

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -1,14 +1,82 @@
 "use client";
+import { Suspense } from "react";
+import Link from "next/link";
+import { ErrorBoundary } from "react-error-boundary";
 
 import { DEFAULT_LIMIT } from "@/constants";
 import { trpc } from "@/trpc/client";
 
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { InfiniteScroll } from "@/components/infinite-scroll";
+
 export const VideosSection = () => {
-  const [data] = trpc.studio.getMany.useSuspenseInfiniteQuery(
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <ErrorBoundary fallback={<p>Error...</p>} />
+      <VideosSectionSuspense />
+    </Suspense>
+  );
+};
+
+const VideosSectionSuspense = () => {
+  const [data, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
     { limit: DEFAULT_LIMIT },
     {
       getNextPageParam: (lastPage) => lastPage.nextCursor,
     },
   );
-  return <div>{JSON.stringify(data)}</div>;
+  return (
+    <div>
+      <div className="border-y">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[510px] pl-6">Video</TableHead>
+              <TableHead>Visibility</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="text-right">Views</TableHead>
+              <TableHead className="text-right">Comments</TableHead>
+              <TableHead className="pr-6 text-right">Likes</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {data.pages.flatMap((page) =>
+              page.items.map((video) => (
+                <Link
+                  href={`/studios/videos/${video.id}`}
+                  key={video.id}
+                  legacyBehavior
+                >
+                  <TableRow className="cursor-pointer">
+                    <TableCell className="w-[510px] pl-6">
+                      {video.title}
+                    </TableCell>
+                    <TableCell>Public</TableCell>
+                    <TableCell>Published</TableCell>
+                    <TableCell>Date</TableCell>
+                    <TableCell className="text-right">100</TableCell>
+                    <TableCell className="text-right">100</TableCell>
+                    <TableCell className="pr-6 text-right">100</TableCell>
+                  </TableRow>
+                </Link>
+              )),
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <InfiniteScroll
+        hasNextPage={query.hasNextPage}
+        isFetchingNextPage={query.isFetchingNextPage}
+        fetchNextPage={query.fetchNextPage}
+      />
+    </div>
+  );
 };
